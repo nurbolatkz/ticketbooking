9ad9ac54e590d56e4431532a5a825e6f0d3cef41
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-01-31 18:14:50 +0600

    add snack card component

--- src/components/SnackItem.jsx
+++ src/components/SnackItem.jsx
@@ -2,28 +2,39 @@ import React, {useState} from 'react';
 
 const SnackItem = (props) => {
 
-    const [quantity, setQuantity] = useState(1)
+    const [quantity, setQuantity] = useState(0);
+
+    const handleIncrement = () => {
+        setQuantity(quantity + 1);
+
+    };
+    const handleDecrement = () => {
+        setQuantity(quantity - 1);
+    };
 
 
     return (
             <div className='snack-card' id={props.id}>
-                <span className='counter'>{props.snack_card.quantity}</span>
-
-                <img src={props.snack_card.img_src} alt=""/>
-                <div className='snack-card-body'>
-                    <div className='snack-card-title'>
-                        <h5>{props.snack_card.snack_name}</h5>
+                <span className={`${quantity >=1 ? "snack-card-counter" : "snack-card-counter-hidden"}`} >
+                    {quantity}
+                </span>
+                <div className={'img-container'}>
+                     <img src={props.snack_card.img_src} alt=""/>
                 </div>
-                    <button className= 'select-btn' onClick={() => props.setVisibility(props.id)}>Выбрать</button>
+
+                <div className='snack-card-body'>
+                    <h4 className="snack-card-title">
+                        {props.snack_card.snack_name} . <span className="snack-card-price">{props.snack_card.price} тг</span>
+                    </h4>
+
+                    <button className= 'select-btn'>Выбрать</button>
 
                     <div className='btn-container'>
-                        <button id='decrement_btn' onClick={() => props.decrement(props.snack_card)}>-</button>
-                        <button id='increment_btn' onClick={() => props.increment(props.snack_card)} >+</button>
+                            <button id='decrement_btn' onClick={handleIncrement} >+</button>
+                            <button id='increment_btn'  onClick={handleDecrement} >-</button>
                     </div>
-
-
+                </div>
             </div>
-        </div>
 
     )
 }
--- src/components/SnackList.jsx
+++ src/components/SnackList.jsx
@@ -2,56 +2,12 @@ import React from 'react';
 import SnackItem from "./SnackItem.jsx";
 
 
-const SnackList = ({snack_cards, setVisibilityCard, increment, decrement}) => {
-    const arrangeByRow = () => {
-
-        let arranged_snack_cards = [];
-        let counter = 0;
-        let row = [];
-        for(let i = 0; i < snack_cards.length; i++){
-
-            if(counter === 3 ){
-                arranged_snack_cards.push(row);
-                counter = 0;
-                row = [];
-            };
-            row.push(snack_cards[i]);
-            if(i === snack_cards.length - 1){
-                arranged_snack_cards.push(row);
-            }
-            counter++;
-
-        }
-        return arranged_snack_cards;
-    }
-
-    const snackMenuArrangedByRow =  arrangeByRow();
-
+const SnackList = ({snack_cards}) => {
     return (
         <div className='snack-container'>
-            {snackMenuArrangedByRow.map((row) =>
+            {snack_cards.map((card) =>
                 <div className = 'row'>
-                    <SnackItem id={'snack-card' + row[0].id} snack_card={row[0]}
-                               setVisibility={setVisibilityCard}
-                               increment = {increment}
-                               decrement = {decrement}
-                    />
-
-                    {row[1] &&
-                        <SnackItem id={'snack-card' + row[1].id} snack_card={row[1]}
-                                   setVisibility={setVisibilityCard}
-                                   increment = {increment}
-                                   decrement = {decrement}
-                        />
-                    }
-
-                    {row[2] &&
-                        <SnackItem id={'snack-card' + row[2].id} snack_card={row[2]}
-                                   setVisibility={setVisibilityCard}
-                                   increment = {increment}
-                                   decrement = {decrement}
-                        />
-                    }
+                    <SnackItem id={'snack-card' + card.id} snack_card={card}></SnackItem>
                 </div>
             )}
         </div>
