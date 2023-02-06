417d62f23750a6412a0dd9018fbd0af5f3c901b5
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-02-06 06:59:24 +0600

    add img src for film block

--- src/components/FilmBlock.jsx
+++ src/components/FilmBlock.jsx
@@ -4,7 +4,7 @@ const FilmBlock = (props) => {
     return(
         <div className='filmBlockBody'>
             <div className='filmInfoBody'>
-                <img className='filmImage' src='babylon.jpg'></img>
+                <img className='filmImage' src='https://i7.imageban.ru/out/2023/02/01/bb07ce15243017c605c7a26f0a027b25.jpg'></img>
                    <div className='filmInfoBlockOne'>
                         <span className='filmName'>{props.movie.film_name}</span><br></br>
                         <span className='filmLang'>Язык: {props.movie.lang}</span>
